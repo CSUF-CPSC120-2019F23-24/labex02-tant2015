58704d5cf194c7526aab350ec84e78e03feb8b93
hnguyen1186 <hnguyen1186@cs104u-4b.acad.fullerton.edu>
2019-08-31 13:32:13 -0700

    done with  problem 1

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,10 +1,18 @@
-// Name: 
+// Name:
 // This program displays a diamond pattern to the screen.
 
 #include <iostream>
 
 int main()
 {
+std :: cout << "   *   \n"; //\n to end the line and i space 3 times before and after  i put *
+std :: cout << "  ***  \n"; // try to line them up 
+std :: cout << " ***** \n";
+std :: cout << "*******\n";
+std :: cout << " ***** \n";
+std :: cout << "  ***  \n";
+std :: cout << "   *   \n";
+
 
   return 0;
 }
